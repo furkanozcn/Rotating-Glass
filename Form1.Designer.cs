05c70f381c462af83acee974a422248b0a5c31b2
Furkan Özcan <fozcan2@hotmail.com>
2018-08-15 14:02:56 +0300

    Computer Graphics Lesson

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Deneme2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenGlControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.buttonX = new System.Windows.Forms.Button();
+            this.buttonY = new System.Windows.Forms.Button();
+            this.buttonZ = new System.Windows.Forms.Button();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelZ = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OpenGlControl
+            // 
+            this.OpenGlControl.AccumBits = ((byte)(0));
+            this.OpenGlControl.AutoCheckErrors = false;
+            this.OpenGlControl.AutoFinish = false;
+            this.OpenGlControl.AutoMakeCurrent = true;
+            this.OpenGlControl.AutoSwapBuffers = true;
+            this.OpenGlControl.BackColor = System.Drawing.Color.Black;
+            this.OpenGlControl.ColorBits = ((byte)(32));
+            this.OpenGlControl.DepthBits = ((byte)(16));
+            this.OpenGlControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.OpenGlControl.Location = new System.Drawing.Point(0, 0);
+            this.OpenGlControl.Name = "OpenGlControl";
+            this.OpenGlControl.Size = new System.Drawing.Size(500, 500);
+            this.OpenGlControl.StencilBits = ((byte)(0));
+            this.OpenGlControl.TabIndex = 0;
+            this.OpenGlControl.Paint += new System.Windows.Forms.PaintEventHandler(this.myPaint);
+            // 
+            // buttonX
+            // 
+            this.buttonX.Location = new System.Drawing.Point(510, 12);
+            this.buttonX.Name = "buttonX";
+            this.buttonX.Size = new System.Drawing.Size(75, 23);
+            this.buttonX.TabIndex = 1;
+            this.buttonX.Text = "X Döndür";
+            this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonKeyDown);
+            this.buttonX.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonKeyUp);
+            this.buttonX.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonX_MouseClick);
+            // 
+            // buttonY
+            // 
+            this.buttonY.Location = new System.Drawing.Point(510, 41);
+            this.buttonY.Name = "buttonY";
+            this.buttonY.Size = new System.Drawing.Size(75, 23);
+            this.buttonY.TabIndex = 2;
+            this.buttonY.Text = "Y Döndür";
+            this.buttonY.UseVisualStyleBackColor = true;
+            this.buttonY.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonKeyDown);
+            this.buttonY.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonKeyUp);
+            this.buttonY.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonY_MouseClick);
+            // 
+            // buttonZ
+            // 
+            this.buttonZ.Location = new System.Drawing.Point(510, 70);
+            this.buttonZ.Name = "buttonZ";
+            this.buttonZ.Size = new System.Drawing.Size(75, 23);
+            this.buttonZ.TabIndex = 3;
+            this.buttonZ.Text = "Z Döndür";
+            this.buttonZ.UseVisualStyleBackColor = true;
+            this.buttonZ.KeyDown += new System.Windows.Forms.KeyEventHandler(this.buttonKeyDown);
+            this.buttonZ.KeyUp += new System.Windows.Forms.KeyEventHandler(this.buttonKeyUp);
+            this.buttonZ.MouseClick += new System.Windows.Forms.MouseEventHandler(this.buttonZ_MouseClick);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(591, 17);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(13, 13);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "0";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(591, 46);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(13, 13);
+            this.labelY.TabIndex = 5;
+            this.labelY.Text = "0";
+            // 
+            // labelZ
+            // 
+            this.labelZ.AutoSize = true;
+            this.labelZ.Location = new System.Drawing.Point(591, 75);
+            this.labelZ.Name = "labelZ";
+            this.labelZ.Size = new System.Drawing.Size(13, 13);
+            this.labelZ.TabIndex = 6;
+            this.labelZ.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 500);
+            this.Controls.Add(this.labelZ);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.buttonZ);
+            this.Controls.Add(this.buttonY);
+            this.Controls.Add(this.buttonX);
+            this.Controls.Add(this.OpenGlControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Bardak";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl OpenGlControl;
+        private System.Windows.Forms.Button buttonX;
+        private System.Windows.Forms.Button buttonY;
+        private System.Windows.Forms.Button buttonZ;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label labelZ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+using ObjLoader.Loader.Loaders;
+using Tao.FreeGlut;
+namespace Deneme2
+{
+    public partial class Form1 : Form
+    {
+        private bool invert = false;
+        private uint[] texture;
+        private string textName = null;
+        private float alfa = 0;
+        private float beta = 0;
+        private float gamma = 0;
+        private LoadResult result;
+
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlControl.InitializeContexts();
+            int height = OpenGlControl.Height;
+            int width = OpenGlControl.Width;
+
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+            float[] light_pos = new float[4] { 1, 0.5F, 1, 0 };
+            Gl.glLightfv(Gl.GL_LIGHT0, Gl.GL_POSITION, light_pos);
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glDepthFunc(Gl.GL_LEQUAL);
+            Gl.glHint(Gl.GL_PERSPECTIVE_CORRECTION_HINT, Gl.GL_NICEST);
+            Gl.glClearColor(0, 0, 0, 1);
+
+            Gl.glViewport(0, 0, width, height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Glu.gluPerspective(45.0f, (double)width / (double)height, 0.01f, 5000.0f);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+            var objLoaderFactory = new ObjLoaderFactory();
+            var objLoader = objLoaderFactory.Create();
+
+            var fileStream = new FileStream("bardak.obj", FileMode.Open, FileAccess.Read);
+            result = objLoader.Load(fileStream);
+        }
+
+
+        private void LoadTexture(string filename)
+        {
+
+            texture = new uint[1];
+            Bitmap image = new Bitmap(filename);
+            image.RotateFlip(RotateFlipType.RotateNoneFlipY);
+            System.Drawing.Imaging.BitmapData bitmapdata;
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            bitmapdata = image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            Gl.glGenTextures(1, texture);
+            Gl.glBindTexture(Gl.GL_TEXTURE_2D, texture[0]);
+            Gl.glTexImage2D(Gl.GL_TEXTURE_2D, 0, (int)Gl.GL_RGB8, image.Width, image.Height,
+                0, Gl.GL_BGR_EXT, Gl.GL_UNSIGNED_BYTE, bitmapdata.Scan0);
+            Gl.glTexParameteri((int)Gl.GL_TEXTURE_2D, (int)Gl.GL_TEXTURE_MIN_FILTER, (int)Gl.GL_LINEAR);
+            Gl.glTexParameteri((int)Gl.GL_TEXTURE_2D, (int)Gl.GL_TEXTURE_MAG_FILTER, (int)Gl.GL_LINEAR);
+
+            image.UnlockBits(bitmapdata);
+            image.Dispose();
+        }
+
+        private void buttonKeyDown(object sender, KeyEventArgs e)
+        {
+            invert = false;
+            if (e.KeyCode == Keys.ShiftKey)
+                invert = true;
+        }
+
+        private void buttonKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey)
+                invert = false;
+        }
+
+        private void buttonX_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (invert)
+                alfa -= 5;
+            else
+                alfa += 5;
+            alfa = (alfa + 360) % 360;
+            labelX.Text = alfa.ToString();
+            OpenGlControl.Refresh();
+        }
+
+        private void buttonY_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (invert)
+                beta -= 5;
+            else
+                beta += 5;
+            beta = (beta + 360) % 360;
+            labelY.Text = beta.ToString();
+            OpenGlControl.Refresh();
+        }
+
+        private void buttonZ_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (invert)
+                gamma -= 5;
+            else
+                gamma += 5;
+            gamma = (gamma + 360) % 360;
+            labelZ.Text = gamma.ToString();
+            OpenGlControl.Refresh();
+        }
+        private void myPaint(object sender, PaintEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glLoadIdentity();
+            Glu.gluLookAt(2, 2, 2, 0, 0, 0, 0, 1, 0);
+
+            Gl.glRotatef(alfa, 1, 0, 0);
+            Gl.glRotatef(beta, 0, 1, 0);
+            Gl.glRotatef(gamma, 0, 0, 1);
+
+            for (int i = 0; i < result.Groups.Count; i++)
+            {
+                var g = result.Groups[i];
+                if (result.Textures.Count > 0)
+                    Gl.glEnable(Gl.GL_TEXTURE_2D);
+                else
+                    Gl.glDisable(Gl.GL_TEXTURE_2D);
+
+                for (int j = 0; j < g.Faces.Count; j++)
+                {
+                    var f = g.Faces[j];
+                    var m = g.Material;
+                    if (m != null)
+                    {
+                        float[] av = { m.AmbientColor.X, m.AmbientColor.Y, m.AmbientColor.Z, 1 };
+                        Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_AMBIENT, av);
+                        float[] dv = { m.DiffuseColor.X, m.DiffuseColor.Y, m.DiffuseColor.Z, 1 };
+                        Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_DIFFUSE, dv);
+                        float[] sv = { m.SpecularColor.X, m.SpecularColor.Y, m.SpecularColor.Z, 1 };
+                        Gl.glMaterialfv(Gl.GL_FRONT, Gl.GL_SPECULAR, sv);
+                        float sh = m.SpecularCoefficient;
+                        Gl.glMaterialf(Gl.GL_FRONT, Gl.GL_SHININESS, sh);
+                        if (m.DiffuseTextureMap != null)
+                        {
+                            if (textName != m.DiffuseTextureMap)
+                                LoadTexture(m.DiffuseTextureMap);
+                        }
+                        textName = m.DiffuseTextureMap;
+                    }
+
+
+                    Gl.glBegin(Gl.GL_POLYGON);
+                    for (int k = 0; k < f.Count; k++)
+                    {
+                        int x = f[k].VertexIndex;
+                        var v = result.Vertices[x - 1];
+                        int y = f[k].NormalIndex;
+                        var n = result.Normals[y - 1];
+
+                        if (textName != null)
+                        {
+                            int z = f[k].TextureIndex;
+                            var t = result.Textures[z - 1];
+                            Gl.glTexCoord2f(t.X, t.Y);
+                        }
+                        Gl.glNormal3f(n.X, n.Y, n.Z);
+                        Gl.glVertex3f(v.X, v.Y, v.Z);
+                    }
+                    Gl.glEnd();
+                }
+            }
+        }
+    }
+}
